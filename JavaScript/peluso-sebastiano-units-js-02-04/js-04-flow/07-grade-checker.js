de6eb1fafadbd8352f53ea48e895dc93882ee912
Sebastiano Peluso <Sebastiano.Peluso@edu-its.it>
2025-05-23 16:52:15 +0200

    add documentation

--- JavaScript/peluso-sebastiano-units-js-02-04/js-04-flow/07-grade-checker.js
+++ JavaScript/peluso-sebastiano-units-js-02-04/js-04-flow/07-grade-checker.js
@@ -1,5 +1,16 @@
+/**
+ * @author: Sebastiano Peluso
+ * @description scription: Questo script assegna un voto letterale (A, B, C, D, F) a un punteggio numerico
+ *              compreso tra 60 e 90 utilizzando una funzione di classificazione.
+ *              Per ogni punteggio nel range specificato, stampa il risultato in console.
+ */
 
-
+/**
+ * Assegna un voto in lettere basato su un punteggio numerico.
+ * 
+ * @param {number} score = 80.
+ * @returns {string} 80.
+ */
 function assignGrade(score) {
   if (score >= 90) {
     return 'A';
@@ -14,8 +25,7 @@ function assignGrade(score) {
   }
 }
 
-
-
+// Cicla dal punteggio 60 al 90 e stampa il voto assegnato
 for (let score = 60; score <= 90; score++) {
   const grade = assignGrade(score);
   console.log(`For ${score}, you got a ${grade}.`);
